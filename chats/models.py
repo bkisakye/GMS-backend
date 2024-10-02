509c2135d03482d445af7e41c79ed296627bbb32
bkisakye <bkisakye@baylor-uganda.org>
2024-10-02 12:47:09 +0300

    subgrantee msgs

--- chats/models.py
+++ chats/models.py
@@ -19,3 +19,9 @@ class Message(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['timestamp']
+
+    def __str__(self):
+        return f"Message from {self.sender.organisation_name}"
